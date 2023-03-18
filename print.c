70b6194885d4fc124196ccd2945615f0ae49c306
Sravya <sravyareddyypati@gmail.com>
2023-03-18 11:56:20 +0530

    commited

---
+++ print.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+    int a,b,c;
+    printf("Enter two numbers");
+    scanf("%d %d",&a,&b);
+    int add=a+b;
+    int diff=a-b;
+    int mul=a*b;
+    int div=a/b;
+    int mod=a%b;
+    printf("The sum is %d ",add);
+    printf("The diff is %d ",diff);
+    printf("The mul is %d ",mul);
+    printf("The div is %d ",div);
+    printf("The mod is %d ",mod);
+    
+
+}   
